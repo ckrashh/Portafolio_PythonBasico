e8dd9bc1cfcec3536c38863d92a746a752c7566b
ckrashh <carrillo.gerald.a.s@gmail.com>
2025-07-15 19:45:18 -0400

    Mensajes mas descreptivos y recomendacion

--- IMC.py
+++ IMC.py
@@ -1,17 +1,16 @@
 peso = int(input("Ingrese su peso en kg: "))
 altura = float(input("Ingrese su altura en metros: "))
-edad = int(input("Ingrese su edad: "))
 
 estatura_cuadrada = altura ** 2
 imc = peso / estatura_cuadrada
 
 if imc < 20:
-    print("Bajo peso")
+    print("Estas Bajo peso deberias ir a un nutricionista")
 elif imc >= 20 and imc <= 25:
-    print("Peso normal")
+    print("Estas en Peso normal te estas cuidando bien")
 elif imc > 25:
-    print("Sobrepeso")
+    print("Estas en Sobrepeso deberias ir a un nutricionista para que te de una dieta adecuada")
 elif imc > 30:
-    print("Obesidad")
+    print("Estas en Obesidad deberias ir a un nutricionista para que te de una dieta adecuada y aun medico para que te de un tratamiento adecuado")
 elif imc > 40: 
-    print("Obesidad morbida")        
+    print("Estas en Obesidad deberias ir a un nutricionista para que te de una dieta adecuada y aun medico para que te de un tratamiento adecuado")        
